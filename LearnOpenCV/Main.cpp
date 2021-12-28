693353c074074ec84a36cc1033cded78c28f08b3
吴俊彦 <wujunyan@sandboxol.com>
2021-12-28 21:57:06 +0800

    添加项目文件。

---
+++ LearnOpenCV/Main.cpp
@@ -0,0 +1,18 @@
+#include "Main.h"
+
+#include <opencv2/opencv.hpp>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	bool isClose = true;
+
+	while (isClose) {
+		cv::Mat image(700, 700, CV_32FC3);
+		image.convertTo(image, CV_8UC3, 1.0f);
+		cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+		cv::imshow("image", image);
+	}
+
+	return 0;
+}
